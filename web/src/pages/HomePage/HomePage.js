d8afc296be910aaa0b480ed690dc53eacf059e06
Franco Rosa <km115.franco@gmail.com>
2022-07-02 20:49:17 -0500

    Add static pages

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,14 @@
+import { MetaTags } from '@redwoodjs/web'
+
+const HomePage = () => {
+  return (
+    <>
+      <MetaTags title="Home" description="Home page" />
+
+      <h1>HomePage</h1>
+      <p>This is the home page</p>
+    </>
+  )
+}
+
+export default HomePage
